5754f84e4c1384cf0146140725cbeaf8e5bd34be
mokey1422 <15253503932@163.com>
2015-09-09 23:40:51 +0800

    zhang

---
+++ 跑马灯效果/GBLoopView/GBLoopView.h
@@ -0,0 +1,67 @@
+//
+//  GBLoopView.h
+//  跑马灯效果
+//
+//  Created by 张国兵 on 15/7/20.
+//  Copyright (c) 2015年 zhangguobing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//方向
+
+typedef enum
+{
+    GBLoopDirectionLeft,
+    GBLoopDirectionRight,
+}GBLoopDirection;
+
+static CGSize appScreenSize;
+static UIInterfaceOrientation lastOrientation;
+
+@interface GBLoopView : UIView{
+  
+    // 记录
+    int currentIndex;
+    
+    //是否循环（默认为YES）
+    BOOL loops;
+    
+    // 当前状态
+    BOOL running;
+    
+    // 内容载体
+    UILabel *tickerLabel;
+    
+
+}
+
+/**
+ *  跑马灯loop速度
+ */
+@property(nonatomic) float Speed;
+/**
+ *  显示的内容(支持多条数据)
+ */
+@property(nonatomic, retain) NSArray *tickerArrs;
+/**
+ * loop方向(左/右)
+ */
+@property(nonatomic) GBLoopDirection Direction;
+/**
+ *  设置背景颜色
+ */
+-(void)setBackColor:(UIColor *)color;
+/**
+ *  开启
+ */
+-(void)start;
+/**
+ *  暂停
+ */
+-(void)pause;
+/**
+ *  复位
+ */
+-(void)resume;
+@end
